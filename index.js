5a970ef204b9bc20747a56b81abc877699bc9da3
Jesse <jessemutua76@gmail.com>
2022-11-23 09:58:09 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const mondayWork = function(activity = "go to the office") {
+    return ("This Monday, I will" + " " + `${activity}.`)
+}
+mondayWork();
+
+function saturdayFun(activity = "roller-skate") {
+    return ("This Saturday, I want to" + " " + `${activity}` + "!")
+}
+saturdayFun("bathe my dog")
+
+function wrapAdjective(initlzr) {
+    if (initlzr === '*') {
+        const innerFunction = function(name = "special")
+    }
+}
